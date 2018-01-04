9df5decd064681f2df1efd8d22e7962b775ff5b1
wangkun1 <wangkun1@corp.netease.com>
2018-01-04 20:07:06 +0800

    lambda test

---
+++ source/zookeeper-learning/src/main/java/com/soul/java8/Java8Tester.java
@@ -0,0 +1,60 @@
+package com.soul.java8;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Function;
+import java.util.function.Predicate;
+
+/***
+ * @author wangkun1
+ * @version 2018/1/4 
+ */
+public class Java8Tester {
+    public static void main(String args[]) {
+        List<Integer> list = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9);
+//        evalTest(list);
+//        list.forEach(System.out::println);
+        Integer sum = list.stream()
+                .filter(n -> n % 2 == 0)
+                .map(n -> n * 2)
+                .filter(n -> n > 5)
+                .reduce((x, y) -> x + y)
+                .get();
+        System.out.println(sum);
+
+    }
+
+    private static void evalTest(List<Integer> list) {
+        // Predicate<Integer> predicate = n -> true
+        // n 是一个参数传递到 Predicate 接口的 test 方法
+        // n 如果存在则 test 方法返回 true
+
+        System.out.println("输出所有数据:");
+
+        // 传递参数 n
+        eval(list, n -> true);
+
+        // Predicate<Integer> predicate1 = n -> n%2 == 0
+        // n 是一个参数传递到 Predicate 接口的 test 方法
+        // 如果 n%2 为 0 test 方法返回 true
+
+        System.out.println("输出所有偶数:");
+        eval(list, n -> n % 2 == 0);
+
+        // Predicate<Integer> predicate2 = n -> n > 3
+        // n 是一个参数传递到 Predicate 接口的 test 方法
+        // 如果 n 大于 3 test 方法返回 true
+
+        System.out.println("输出大于 3 的所有数字:");
+        eval(list, n -> n > 3);
+    }
+
+    public static void eval(List<Integer> list, Predicate<Integer> predicate) {
+        for (Integer n : list) {
+
+            if (predicate.test(n)) {
+                System.out.println(n + " ");
+            }
+        }
+    }
+}
--- source/zookeeper-learning/src/main/java/com/soul/zookeeper/curator/d_LeaderSelector.java
+++ source/zookeeper-learning/src/main/java/com/soul/zookeeper/curator/d_LeaderSelector.java
@@ -9,31 +9,47 @@ import org.apache.curator.framework.recipes.leader.LeaderSelectorListenerAdapter
 import org.apache.curator.framework.state.ConnectionState;
 import org.apache.curator.retry.ExponentialBackoffRetry;
 
+import java.util.concurrent.CountDownLatch;
+
 /***
  * @author wangkun1
  * @version 2018/1/2 
  */
 public class d_LeaderSelector {
 
-    static CuratorFramework client = CuratorFrameworkFactory.builder()
-            .connectString(Constants.ZOOKEEPER_FAKE_CLUSTER_SERVER_PATH)
-            .connectionTimeoutMs(5000)
-            .retryPolicy(new ExponentialBackoffRetry(1000,3))
-            .build();
+    static CountDownLatch countDownLatch = new CountDownLatch(1);
 
     public static void main(String[] args) throws InterruptedException {
-        client.start();
-        LeaderSelector selector = new LeaderSelector(client, Constants.ROOT_PATH, new LeaderSelectorListenerAdapter() {
-            @Override
-            public void takeLeadership(CuratorFramework curatorFramework) throws Exception {
-                System.out.println("Become new Master!");
-                Thread.sleep(3000);
-                System.out.println("Finish Master Operation , release lock");
-            }
-        });
 
-        selector.autoRequeue();
-        selector.start();
+        for (int i = 0; i < 10; i++) {
+            new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    CuratorFramework client = CuratorFrameworkFactory.builder()
+                            .connectString(Constants.ZOOKEEPER_FAKE_CLUSTER_SERVER_PATH)
+                            .connectionTimeoutMs(5000)
+                            .retryPolicy(new ExponentialBackoffRetry(1000, 3))
+                            .build();
+                    client.start();
+                    LeaderSelector selector = new LeaderSelector(client, Constants.ROOT_PATH, new LeaderSelectorListenerAdapter() {
+                        @Override
+                        public void takeLeadership(CuratorFramework curatorFramework) throws Exception {
+                            System.out.println(Thread.currentThread().getName() + " Become new Master!");
+                            Thread.sleep(3000);
+                            System.out.println(Thread.currentThread().getName() + " Finish Master Operation , release lock");
+                        }
+                    });
+                    selector.autoRequeue();
+                    try {
+                        countDownLatch.await();
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                    selector.start();
+                }
+            }).start();
+        }
+        countDownLatch.countDown();
         Thread.sleep(Integer.MAX_VALUE);
     }
 }
