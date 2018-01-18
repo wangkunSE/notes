8df2260401e38bbf67533b5e31fdb594692c4e8b
wangkun1 <wangkun1@corp.netease.com>
2018-01-18 21:11:15 +0800

    update zookeeper

--- source/zookeeper-learning/springboothelloworld/src/main/java/com/soul/service/UserService.java
+++ source/zookeeper-learning/springboothelloworld/src/main/java/com/soul/service/UserService.java
@@ -3,8 +3,13 @@ package com.soul.service;
 import com.soul.dao.UserDao;
 import com.soul.domain.User;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
+import org.springframework.core.io.support.ResourcePatternResolver;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.io.IOException;
 
 /***
  * @author wangkun1
@@ -33,6 +38,16 @@ public class UserService {
                 System.out.print("--");
             }
             System.out.println("CurrentClassLoader : " + currentClassLoader);
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        ResourcePatternResolver resolver = new PathMatchingResourcePatternResolver();
+        Resource[] resources = resolver.getResources("classpath*:*.*");
+
+        assert resources != null;
+        for (Resource resource : resources) {
+            System.out.println(resource.getDescription());
         }
     }
 }
